3582e6490d3160a9fc762ae5bcde3d3fd476800f
Tracy Ngo <tngo1206@gmail.com>
2019-04-19 01:41:39 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,65 +1,65 @@
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
-('Alpine Lodge', 'Recreation', 100000, '6/1/16', '12/1/16'),
-('Banana Pies', 'Food', 200000, '9/1/16', '12/1/16'),
-('Camel Racing', 'Recreation', 500000, '12/1/16', '12/1/17'),
-('Duck Breeding', 'Book', 50000, '6/15/16', '8/15/16'),
-('Elephant Ears', 'Music', 25000, '7/15/16', '12/15/16'),
-('Franks on a Stick', 'Food', 1000000, '6/1/16', '6/1/17'),
-('Goose in a Box', 'Animals', 5000, '4/1/16', '11/1/16'),
-('Hall of Fame', 'Music', 100000, '6/1/16', '12/1/16'),
-('I Wrote a book on SQL', 'Book', 5000, '9/1/16', '10/1/16'),
-("Just kidding, I didn't", 'Book', 6000, '10/1/16', '12/1/16');
+INSERT INTO users (id, name, age) VALUES (1, 'Neet', 17);
+INSERT INTO users (id, name, age) VALUES (2, 'Haxors', 6);
+INSERT INTO users (id, name, age) VALUES (3, 'NEWB', 4);
+INSERT INTO users (id, name, age) VALUES (4, 'WOWzers', 33);
+INSERT INTO users (id, name, age) VALUES (5, 'Mahougician', 24);
+INSERT INTO users (id, name, age) VALUES (6, 'Tohru', 24);
+INSERT INTO users (id, name, age) VALUES (7, 'Rosey', 9);
+INSERT INTO users (id, name, age) VALUES (8, 'Kazuma', 23);
+INSERT INTO users (id, name, age) VALUES (9, 'Lalatina', 100);
+INSERT INTO users (id, name, age) VALUES (10, 'Aqua', 30);
+INSERT INTO users (id, name, age) VALUES (11, 'Megumin', 90);
+INSERT INTO users (id, name, age) VALUES (12, 'Remiru', 24);
+INSERT INTO users (id, name, age) VALUES (13, 'Kobayashi', 4);
+INSERT INTO users (id, name, age) VALUES (14, 'Rem', 36);
+INSERT INTO users (id, name, age) VALUES (15, 'Valdera', 113);
+INSERT INTO users (id, name, age) VALUES (16, 'Kon', 5);
+INSERT INTO users (id, name, age) VALUES (17, 'Ash', 6);
+INSERT INTO users (id, name, age) VALUES (18, 'Squirtle', 5);
+INSERT INTO users (id, name, age) VALUES (19, 'Charmander', 24);
+INSERT INTO users (id, name, age) VALUES (20, 'Bulbasaur', 24);
 
- INSERT INTO users (name, age) VALUES
-('Finnebar', 17),
-('Bear', 6),
-('Iguana', 4),
-('Alex', 33),
-('Amanda', 24),
-('Sophie', 24),
-('Rosey', 9),
-('Victoria', 23),
-('Franz', 100),
-('Hermione', 30),
-('Voldemort', 90),
-('Marisa', 24),
-('Swizzle', 4),
-('Sirius', 36),
-('Albus', 113),
-('Squid', 5),
-('Whale', 6),
-('Pacha', 5),
-('Ena', 24),
-('Katie', 24);
 
- INSERT INTO pledges (amount, user_id, project_id) VALUES
-(10.00, 1, 2),
-(20.00, 1, 3),
-(40.00, 1, 4),
-(50.00, 2, 3),
-(10.00, 3, 2),
-(20.00, 4, 4),
-(40.00, 5, 10),
-(60.00, 6, 10),
-(50.00, 7, 9),
-(700.00, 8, 8),
-(1000.00, 8, 7),
-(40.00, 9, 6),
-(50.00, 9, 3),
-(50.00, 10, 4),
-(24.00, 12, 1),
-(34.00, 11, 1),
-(12.00, 13, 6),
-(19.00, 14, 5),
-(20.00, 15, 5),
-(40.00, 16, 6),
-(35.50, 17, 7),
-(40.00, 18, 8),
-(60.00, 19, 9),
-(70.00, 20, 10),
-(100.00, 20, 4),
-(40.00, 19, 1),
-(20.00, 18, 6),
-(90.00, 17, 9),
-(230.00, 16, 6),
-(450.00, 15, 5);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, 'Magical Music', 'music', 500.00, '2013-06-30', '2013-07-30'),
+(2, 'The Time I got Reincarnated as a Slime', 'books', 20.00, '2013-01-01', '2014-01-01'),
+(3, 'Winning at Legolases Legoland','books',1000.00, '2013-05-30', '2015-05-30'),
+(4, 'Gear up the Noobs', 'charity', 400.00, '2013-03-14', '2013-06-30'),
+(5, 'Donate your gold for the guild', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
+(6, 'Afterlife Meolodies', 'music', 200.00, '2014-12-30', '2015-12-30'),
+(7, 'Pet Dragon needs food', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
+(8, 'Dragon needs a bath', 'charity', 800.00, '2014-06-30', '2014-06-31'),
+(9, 'Where am I? Help me!', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
+(10, 'Chickens for food', 'charity', 5000.00, '2012-03-20', '2013-06-30');
+
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
+(1, 10.00, 1, 2),
+(2, 20.00, 1, 3),
+(3, 40.00, 1, 4),
+(4, 50.00, 2, 3),
+(5, 10.00, 3, 2),
+(6, 20.00, 4, 4),
+(7, 40.00, 5, 10),
+(8, 60.00, 6, 10),
+(9, 50.00, 7, 9),
+(10, 700.00, 8, 8),
+(11, 1000.00, 8, 7),
+(12, 40.00, 9, 6),
+(13, 50.00, 9, 3),
+(14, 50.00, 10, 4),
+(15, 24.00, 12, 1),
+(16, 34.00, 11, 1),
+(17, 12.00, 13, 6),
+(18, 19.00, 14, 5),
+(19, 20.00, 15, 5),
+(20, 40.00, 16, 6),
+(21, 35.50, 17, 7),
+(22, 40.00, 18, 8),
+(23, 60.00, 19, 9),
+(24, 70.00, 20, 10),
+(25, 100.00, 20, 4),
+(26, 40.00, 19, 1),
+(27, 20.00, 18, 6),
+(28, 90.00, 17, 9),
+(29, 230.00, 16, 6),
+(30, 450.00, 15, 5);
